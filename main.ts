6c37135d4b6e6d084b7584dc7a5c0d0d0a8b692b
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-09-22 14:12:26 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        # # # # #
+        . . . # #
+        . . . # #
+        `)
 })
